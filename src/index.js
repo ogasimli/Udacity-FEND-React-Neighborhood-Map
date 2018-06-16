7ac81a9d608fd88365083bf0214a46a726090aba
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2018-06-17 00:55:13 +0400

    refactor: Reorganize folders

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './main/res/styles/index.css';
+import App from './main/js/components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/main/js/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import MapContainer from './MapContainer';
+import '../../res/styles/App.css';
+
+class App extends Component {
+  render() {
+    return <MapContainer />;
+  }
+}
+
+export default App;
