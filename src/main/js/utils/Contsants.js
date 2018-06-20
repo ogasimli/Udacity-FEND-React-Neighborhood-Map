41f46a539205b6fe6e08309cae8553e474bc89bf
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2018-06-20 18:40:32 +0400

    feat: Add places categories as constant

---
+++ src/main/js/utils/Contsants.js
@@ -0,0 +1,38 @@
+/**
+ * Object that holds id and names of our place categories
+ */
+export const categories = [
+  {
+    id: 'historic_monuments',
+    title: 'Historic Monument',
+    icon:
+      'https://www.google.com/maps/vt/icon/name=assets/icons/poi/tactile/pinlet_shadow-1-small.png,assets/icons/poi/tactile/pinlet_outline_v2-1-small.png,assets/icons/poi/tactile/pinlet-1-small.png,assets/icons/poi/quantum/pinlet/historic_pinlet-1-small.png&highlight=ff000000,ffffff,13b5c7,ffffff&color=ff000000?scale=1.25'
+  },
+  {
+    id: 'park',
+    title: 'Park',
+    icon:
+      'https://www.google.com/maps/vt/icon/name=assets/icons/poi/tactile/pinlet_shadow-2-medium.png,assets/icons/poi/tactile/pinlet_outline_v2-2-medium.png,assets/icons/poi/tactile/pinlet-2-medium.png,assets/icons/poi/quantum/pinlet/cemetery_pinlet-2-medium.png&highlight=ff000000,ffffff,4db546,ffffff&color=ff000000?scale=1.25'
+  },
+  {
+    id: 'museum',
+    title: 'Museum',
+    icon:
+      'https://www.google.com/maps/vt/icon/name=assets/icons/poi/tactile/pinlet_shadow-1-small.png,assets/icons/poi/tactile/pinlet_outline_v2-1-small.png,assets/icons/poi/tactile/pinlet-1-small.png,assets/icons/poi/quantum/pinlet/museum_pinlet-1-small.png&highlight=ff000000,ffffff,13b5c7,ffffff&color=ff000000?scale=1.25'
+  },
+  {
+    id: 'music_hall',
+    title: 'Music Hall',
+    icon:
+      'https://www.google.com/maps/vt/icon/name=assets/icons/poi/tactile/pinlet_shadow-2-medium.png,assets/icons/poi/tactile/pinlet_outline_v2-2-medium.png,assets/icons/poi/tactile/pinlet-2-medium.png,assets/icons/poi/quantum/pinlet/note_pinlet-2-medium.png&highlight=ff000000,ffffff,13b5c7,ffffff&color=ff000000?scale=1.25'
+  },
+  {
+    id: 'sports_hall',
+    title: 'Sports Hall',
+    icon:
+      'https://www.google.com/maps/vt/icon/name=assets/icons/poi/tactile/pinlet_shadow-2-medium.png,assets/icons/poi/tactile/pinlet_outline_v2-2-medium.png,assets/icons/poi/tactile/pinlet-2-medium.png,assets/icons/poi/quantum/pinlet/stadium_pinlet-2-medium.png&highlight=ff000000,ffffff,4db546,ffffff&color=ff000000?scale=1.25'
+  }
+];
+
+export const spotLightIcon =
+  'https://www.google.com/maps/vt/icon/name=assets/icons/spotlight/spotlight_pin_v2_shadow-1-small.png,assets/icons/spotlight/spotlight_pin_v2-1-small.png,assets/icons/spotlight/spotlight_pin_v2_dot-1-small.png,assets/icons/spotlight/spotlight_pin_v2_accent-1-small.png&highlight=ff000000,ea4335,960a0a,ff000000&color=ff000000?scale=2';
