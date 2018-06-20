beb90d35c392e22f43db63efeaa9a4fca95329ae
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2018-06-20 18:41:09 +0400

    feat: Add method to fetch all places from places.json file

---
+++ src/main/js/utils/PlacesAPI.js
@@ -0,0 +1,21 @@
+/**
+ * Get URL of the paces.json file on your server.
+ *
+ * @returns {String} - URL of the file
+ */
+const PLACES_URL = () => {
+  const port = 3000; // Change this to your server port
+  return `http://localhost:${port}/data/places.json`;
+};
+
+/**
+ * Fetch all places from local JSON file
+ *
+ * @returns {array} - array of places
+ */
+export const getAll = () =>
+  fetch(PLACES_URL())
+    .then(res => res.json())
+    .then(places => {
+      console.log('places:', places);
+    });
