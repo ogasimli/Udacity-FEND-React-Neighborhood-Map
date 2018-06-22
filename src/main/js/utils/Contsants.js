f38f04d241d6558c2569b9b102ce312b874705e2
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2018-06-22 18:02:29 +0400

    fix: Fix marker icons and change their color

--- src/main/js/utils/Contsants.js
+++ src/main/js/utils/Contsants.js
@@ -6,33 +6,31 @@ export const categories = [
     id: 'historic_monument',
     title: 'Monuments',
     markerIcon:
-      'https://www.google.com/maps/vt/icon/name=assets/icons/poi/tactile/pinlet_shadow-1-small.png,assets/icons/poi/tactile/pinlet_outline_v2-1-small.png,assets/icons/poi/tactile/pinlet-1-small.png,assets/icons/poi/quantum/pinlet/historic_pinlet-1-small.png&highlight=ff000000,c374ff,8c43ff,ffffff&color=ff000000?scale=1.75'
+      'https://www.google.com/maps/vt/icon/name=assets/icons/poi/tactile/pinlet_shadow-2-medium.png,assets/icons/poi/tactile/pinlet_outline_v2-2-medium.png,assets/icons/poi/tactile/pinlet-2-medium.png,assets/icons/poi/quantum/pinlet/historic_pinlet-2-medium.png&highlight=ff000000,ffffff,4CAF50,ffffff&color=ff000000?scale=1.5'
   },
   {
     id: 'park',
     title: 'Parks',
     markerIcon:
-      'https://www.google.com/maps/vt/icon/name=assets/icons/poi/tactile/pinlet_shadow-2-medium.png,assets/icons/poi/tactile/pinlet_outline_v2-2-medium.png,assets/icons/poi/tactile/pinlet-2-medium.png,assets/icons/poi/quantum/pinlet/cemetery_pinlet-2-medium.png&highlight=ff000000,c374ff,8c43ff,ffffff&color=ff000000?scale=1.5'
+      'https://www.google.com/maps/vt/icon/name=assets/icons/poi/tactile/pinlet_shadow-2-medium.png,assets/icons/poi/tactile/pinlet_outline_v2-2-medium.png,assets/icons/poi/tactile/pinlet-2-medium.png,assets/icons/poi/quantum/pinlet/cemetery_pinlet-2-medium.png&highlight=ff000000,ffffff,4CAF50,ffffff&color=ff000000?scale=1.5'
   },
   {
     id: 'museum',
     title: 'Museums',
     markerIcon:
-      'https://www.google.com/maps/vt/icon/name=assets/icons/poi/tactile/pinlet_shadow-1-small.png,assets/icons/poi/tactile/pinlet_outline_v2-1-small.png,assets/icons/poi/tactile/pinlet-1-small.png,assets/icons/poi/quantum/pinlet/museum_pinlet-1-small.png&highlight=ff000000,c374ff,8c43ff,ffffff&color=ff000000?scale=1.75'
+      'https://www.google.com/maps/vt/icon/name=assets/icons/poi/tactile/pinlet_shadow-2-medium.png,assets/icons/poi/tactile/pinlet_outline_v2-2-medium.png,assets/icons/poi/tactile/pinlet-2-medium.png,assets/icons/poi/quantum/pinlet/museum_pinlet-2-medium.png&highlight=ff000000,ffffff,4CAF50,ffffff&color=ff000000?scale=1.5'
   },
   {
     id: 'music_hall',
     title: 'Music Halls',
     markerIcon:
-      'https://www.google.com/maps/vt/icon/name=assets/icons/poi/tactile/pinlet_shadow-2-medium.png,assets/icons/poi/tactile/pinlet_outline_v2-2-medium.png,assets/icons/poi/tactile/pinlet-2-medium.png,assets/icons/poi/quantum/pinlet/note_pinlet-2-medium.png&highlight=ff000000,c374ff,8c43ff,ffffff&color=ff000000?scale=1.5'
+      'https://www.google.com/maps/vt/icon/name=assets/icons/poi/tactile/pinlet_shadow-2-medium.png,assets/icons/poi/tactile/pinlet_outline_v2-2-medium.png,assets/icons/poi/tactile/pinlet-2-medium.png,assets/icons/poi/quantum/pinlet/note_pinlet-2-medium.png&highlight=ff000000,ffffff,4CAF50,ffffff&color=ff000000?scale=1.5'
   },
   {
     id: 'sports_hall',
     title: 'Sports Halls',
     markerIcon:
-      'https://www.google.com/maps/vt/icon/name=assets/icons/poi/tactile/pinlet_shadow-2-medium.png,assets/icons/poi/tactile/pinlet_outline_v2-2-medium.png,assets/icons/poi/tactile/pinlet-2-medium.png,assets/icons/poi/quantum/pinlet/stadium_pinlet-2-medium.png&highlight=ff000000,c374ff,8c43ff,ffffff&color=ff000000?scale=1.5'
+      'https://www.google.com/maps/vt/icon/name=assets/icons/poi/tactile/pinlet_shadow-2-medium.png,assets/icons/poi/tactile/pinlet_outline_v2-2-medium.png,assets/icons/poi/tactile/pinlet-2-medium.png,assets/icons/poi/quantum/pinlet/stadium_pinlet-2-medium.png&highlight=ff000000,ffffff,4CAF50,ffffff&color=ff000000?scale=1.5'
   }
 ];
 
-export const spotLightIcon =
-  'https://www.google.com/maps/vt/icon/name=assets/icons/spotlight/spotlight_pin_v2_shadow-1-small.png,assets/icons/spotlight/spotlight_pin_v2-1-small.png,assets/icons/spotlight/spotlight_pin_v2_dot-1-small.png,assets/icons/spotlight/spotlight_pin_v2_accent-1-small.png&highlight=ff000000,c374ff,8c43ff,ff000000&color=ff000000?scale=2';
